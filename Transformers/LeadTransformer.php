f4b99f62a1d5f16da97b72fb7af8beae5923dfe0
msolanorenuity <msolano@contractor.renuityhome.com>
2025-01-16 18:52:29 -0500

    fixed issue with lead values

--- Transformers/LeadTransformer.php
+++ Transformers/LeadTransformer.php
@@ -7,24 +7,25 @@ use Modules\Core\Icrud\Transformers\CrudResource;
 class LeadTransformer extends CrudResource
 {
   /**
-  * Method to merge values with response
-  *
-  * @return array
-  */
+   * Method to merge values with response
+   *
+   * @return array
+   */
   public function modelAttributes($request)
   {
 
-    
+
     $form = $this->form;
     $fields = $form->fields ?? [];
 
-    foreach($fields as $field){
-      if($field->type == 12) { //FILE
-        $data["values"][$field->name] = url($data["values"][$field->name] ?? '');
-      }
+    $data = ["values" => []];
+    foreach ($fields as $field) {
+      if ($field->type == 12) { //FILE
+        $data["values"][$field->name] = url($this->values[$field->name] ?? '');
+      }else $data["values"][$field->name] = $this->values[$field->name] ?? '';
     }
-     
-    return $data ?? [];
+
+    return $data;
 
   }
 }
